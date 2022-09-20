3b29d9ef9e58c14f8f681a9742776f6186a94cd1
Stavitskii <pochta100@mail.ru>
2022-09-20 08:19:31 +0300

    -2:09:43

--- equipment.py
+++ equipment.py
@@ -8,22 +8,29 @@ import json
 
 @dataclass
 class Armor:
-    pass
+    id: int
+    name: str
+    defence: float
+    stamina_per_turn: float
 
 
 @dataclass
 class Weapon:
-    pass
+    id: int
+    name: str
+    min_damage: float
+    max_damage: float
+    stamina_per_hit: float
 
     @property
     def damage(self):
-        pass
+        return round(uniform(self.min_damage, self.max_damage), 1)
 
 
 @dataclass
 class EquipmentData:
-    # TODO содержит 2 списка - с оружием и с броней
-    pass
+    weapons: list[Weapon]
+    armors: list[Armor]
 
 
 class Equipment:
@@ -50,9 +57,11 @@ class Equipment:
     @staticmethod
     def _get_equipment_data() -> EquipmentData:
         # TODO этот метод загружает json в переменную EquipmentData
-        equipment_file = open("./data/equipment.json")
-        data = json.load( ... )
-        equipment_schema = marshmallow_dataclass.class_schema( ... )
+
+        with open("./data/equipment.json") as f:
+            equipment_file = f
+            data = json.load(equipment_file)
+        equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
         try:
             return equipment_schema().load(data)
         except marshmallow.exceptions.ValidationError:
