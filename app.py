60b01f613c64c66bbafa03845d4f9fd31d774343
Stavitskii <pochta100@mail.ru>
2022-09-21 18:00:33 +0300

    ~20

--- app.py
+++ app.py
@@ -74,9 +74,26 @@ def choose_hero():
         heroes['player'] = new_player
         return redirect(url_for("choose_enemy"))
 
+
 @app.route("/choose-enemy/", methods=['post', 'get'])
 def choose_enemy():
-    # TODO кнопка выбор соперников. 2 метода GET и POST
-    # TODO также на GET отрисовываем форму.
-    # TODO а на POST отправляем форму и делаем редирект на начало битвы
-    pass
+    if request.method == 'GET':
+        header = 'Choose an Enemy'
+        equipment = Equipment()
+        weapons = equipment.get_weapons_names()
+        armors = equipment.get_armors_names()
+        classes = unit_classes
+        return render_template('hero_choosing.html',
+                               result={'header': header, 'weapons': weapons, 'armors': armors, 'classes': classes})
+    if request.method == 'POST':
+        name = request.form['name']
+        chosen_unit_class = request.form['chosen_unit_class']
+        weapon_name = request.form['weapon_name']
+        armor_name = request.form['armor_name']
+        new_enemy = EnemyUnit(name=name, unit_class=unit_classes[chosen_unit_class])
+        new_enemy.equip_armor(Equipment().get_armor(armor_name))
+        new_enemy.equip_weapon(Equipment().get_weapon(weapon_name))
+        heroes['enemy'] = new_enemy
+        return redirect(url_for("start_fight"))
+if __name__ == "__main__":
+    app.run()
